2c35c9d6cc2a2bb8de5998c5ff0014406984e423
Jeff <culbair@gmail.com>
2025-08-18 17:12:10 -0400

    Updated backup.sh to use encrypt_aes.sh

--- backup.sh
+++ backup.sh
@@ -2,8 +2,9 @@
 from datetime import datetime
 import subprocess
 
+subprocess.call(["sudo", "rm", "-rf", "config/nest/event_media"])
+
 fn = 'home-assistant' + datetime.today().strftime('%Y-%m-%d') + '.tz'
 subprocess.call(["sudo", "tar", "cvzf", fn, "config", "matter-server6", "create.sh", "backup.sh"])
-#subprocess.call(["encrypt.sh", fn])
-#subprocess.call(["mv", fn + ".des", "/mnt/backup"])
-#subprocess.call(["rm", fn])
+subprocess.call(["encrypt_aes.sh", fn])
+subprocess.call(["rm", fn])
