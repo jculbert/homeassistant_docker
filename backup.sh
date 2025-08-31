f8c4ac9a3d4c147014793cbad95a1a9cdc46138f
Jeff <culbair@gmail.com>
2025-08-31 17:14:36 -0400

    Updated to 2025.8.3

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 from datetime import datetime
 import subprocess
 
-subprocess.call(["sudo", "rm", "-rf", "config/nest/event_media"])
+#subprocess.call(["sudo", "rm", "-rf", "config/nest/event_media"])
 
 fn = 'home-assistant' + datetime.today().strftime('%Y-%m-%d') + '.tz'
 subprocess.call(["sudo", "tar", "cvzf", fn, "config", "matter-server6", "create.sh", "backup.sh"])
--- create.sh
+++ create.sh
@@ -1,1 +1,1 @@
-docker run -d --name="home-assistant" -v /home/auto/builds/home-assistant/config:/config -v /etc/localtime:/etc/localtime:ro  --net=host --restart=no homeassistant/home-assistant:2024.5.4
+docker run -d --name="home-assistant" -v /home/auto/builds/home-assistant/config:/config -v /etc/localtime:/etc/localtime:ro  --net=host --restart=no homeassistant/home-assistant:2025.8.3
